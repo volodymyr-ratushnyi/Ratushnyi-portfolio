136b9208ab7c3866f54ebcad98efa5e582ba2fa2
driverwolf <driverwolf@gmail.com>
2021-03-23 20:11:28 +0200

    deploy second

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname + '/dist/ratuha'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+res.sendFile(path.join(__dirname+'/dist/ratuha/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
